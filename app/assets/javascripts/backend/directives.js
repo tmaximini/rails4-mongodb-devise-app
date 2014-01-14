d1ea0da5eed7d7f5b184ae518150e0b1826be5c6
tmaximini <tmaximini@gmail.com>
2014-01-14 17:11:14 +0100

    styling

---
+++ app/assets/javascripts/backend/directives.js
@@ -0,0 +1,17 @@
+angular.module('storiesApp')
+
+  .directive('confirmationNeeded', function () {
+  return {
+    priority: 1,
+    terminal: true,
+    link: function (scope, element, attr) {
+      var msg = attr.confirmationNeeded || "Are you sure?";
+      var clickAction = attr.ngClick;
+      element.bind('click',function () {
+        if ( window.confirm(msg) ) {
+          scope.$eval(clickAction);
+        }
+      });
+    }
+  };
+});
